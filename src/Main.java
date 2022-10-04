15a39db9dc89e76dec2a220bed76e98a78d152d6
Роман <pangujailbreake58@gmail.com>
2022-10-04 21:29:19 +0300

    первая часть

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=10;i++)
+        System.out.println(i);
+        for (int i=10; i>=1;i--)
+            System.out.println(i);
+        for (int i=0; i<=17;i=i+2)
+            System.out.println(i);
+        for (int i=10; i>=-10;i--)
+            System.out.println(i);
+    }
+}
